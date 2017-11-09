ebca15b982efc49152091ea1e251051c1cca6b66
fanhl <fanhonglin@klicen.com>
2017-11-09 16:33:27 +0800

    ?

--- app/src/main/java/com/fanhl/architecturedemo/MainActivity.kt
+++ app/src/main/java/com/fanhl/architecturedemo/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.fanhl.architecturedemo
 
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
@@ -12,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate ")
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
@@ -19,6 +24,8 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        val logLife = LogLife()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,5 +42,38 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+}
+
+/**
+ * 测试 LifecycleObserver
+ */
+class LogLife : LifecycleObserver {
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate() {
+        Log.d(TAG, "onCreate ")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume() {
+        Log.d(TAG, "onResume ")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPause() {
+        Log.d(TAG, "onPause ")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+        Log.d(TAG, "donDestory ")
+    }
+
+    companion object {
+        private val TAG = LogLife::class.java.simpleName
     }
 }
