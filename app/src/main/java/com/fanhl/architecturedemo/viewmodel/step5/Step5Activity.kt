b8dd7f1c2e4f3269bd5b74bc7b0ffb64e1bc826f
fanhl <fanhonglin@klicen.com>
2017-11-10 14:36:55 +0800

    step 5

---
+++ app/src/main/java/com/fanhl/architecturedemo/viewmodel/step5/Step5Activity.kt
@@ -0,0 +1,13 @@
+package com.fanhl.architecturedemo.viewmodel.step5
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.fanhl.architecturedemo.R
+
+class Step5Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step5)
+    }
+}
