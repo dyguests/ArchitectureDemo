34289b53dc4328dba52b49d9eb7addf316acf1d4
fanhl <fanhonglin@klicen.com>
2017-11-10 10:45:13 +0800

    step 3

--- app/src/main/java/com/fanhl/architecturedemo/viewmodel/step2/Chrono2Activity.kt
+++ app/src/main/java/com/fanhl/architecturedemo/viewmodel/step2/Chrono2Activity.kt
@@ -1,4 +1,4 @@
-package com.fanhl.architecturedemo.viewmodel
+package com.fanhl.architecturedemo.viewmodel.step2
 
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProviders
@@ -6,14 +6,13 @@ import android.os.Bundle
 import android.os.SystemClock
 import android.support.v7.app.AppCompatActivity
 import com.fanhl.architecturedemo.R
-import kotlinx.android.synthetic.main.activity_chrono2.*
-import kotlinx.android.synthetic.main.content_chrono2.*
+import kotlinx.android.synthetic.main.activity_chrono.*
 
 class Chrono2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chrono2)
+        setContentView(R.layout.activity_chrono)
         setSupportActionBar(toolbar)
 
         val viewModel = ViewModelProviders.of(this).get(ChronometerViewModel::class.java)
---
+++ app/src/main/java/com/fanhl/architecturedemo/viewmodel/step3/Chrono3Activity.kt
@@ -0,0 +1,31 @@
+package com.fanhl.architecturedemo.viewmodel.step3
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.os.SystemClock
+import android.support.v7.app.AppCompatActivity
+import com.fanhl.architecturedemo.R
+import kotlinx.android.synthetic.main.activity_chrono.*
+
+class Chrono3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chrono)
+        setSupportActionBar(toolbar)
+
+        val viewModel = ViewModelProviders.of(this).get(ChronometerViewModel::class.java)
+
+        viewModel.startDate ?: run { viewModel.startDate = SystemClock.elapsedRealtime() }
+        chronometer.base = viewModel.startDate!!
+
+        chronometer.start()
+    }
+}
+
+class ChronometerViewModel(
+        var startDate: Long?
+) : ViewModel() {
+    constructor() : this(null)
+}
