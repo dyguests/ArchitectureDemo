c19e3a5f8847d0c28614a0f390e2e99c76cd7b92
fanhl <fanhonglin@klicen.com>
2017-11-13 10:18:10 +0800

    Rxjava 2 with Android Architecture components

---
+++ app/src/main/java/com/fanhl/architecturedemo/rxjava2/Rxjava2Activity.kt
@@ -0,0 +1,23 @@
+package com.fanhl.architecturedemo.rxjava2
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import com.fanhl.architecturedemo.R
+
+import kotlinx.android.synthetic.main.activity_rxjava2.*
+
+class Rxjava2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rxjava2)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
