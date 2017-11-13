fc5e15490f254f4132e8f4759390c5ba79cf5557
fanhl <fanhonglin@klicen.com>
2017-11-13 10:50:20 +0800

    RxliveData

---
+++ app/src/main/java/com/fanhl/architecturedemo/rxjava2/RxLiveData.kt
@@ -0,0 +1,26 @@
+package com.fanhl.architecturedemo.rxjava2
+
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.MutableLiveData
+import com.fanhl.architecturedemo.extensions.observe
+import io.reactivex.Observable
+
+/**
+ * desc:
+ * date: 2017/11/13
+ *
+ * @author fanhl
+ */
+class RxLiveData<T> : MutableLiveData<T>() {
+    fun asObservable(owner: LifecycleOwner): Observable<T> {
+        return Observable.create<T> { emitter ->
+            observe(owner) { data ->
+                if (data != null) {
+                    emitter.onNext(data)
+                } else {
+//                    emitter.empty()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/fanhl/architecturedemo/rxjava2/Rxjava2Activity.kt
+++ app/src/main/java/com/fanhl/architecturedemo/rxjava2/Rxjava2Activity.kt
@@ -1,23 +1,32 @@
 package com.fanhl.architecturedemo.rxjava2
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.fanhl.architecturedemo.R
-
 import kotlinx.android.synthetic.main.activity_rxjava2.*
 
 class Rxjava2Activity : AppCompatActivity() {
+    val liveData by lazy { RxLiveData<Long>() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rxjava2)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        fab.setOnClickListener { liveData.value = System.currentTimeMillis() }
+
+        liveData
+                .asObservable(this)
+                .subscribe { Log.i(TAG, "case1 data:$it") }
+        liveData
+                .asObservable(this)
+        liveData
+                .asObservable(this)
+                .subscribe { Log.i(TAG, "case2 data:$it") }
     }
 
+    companion object {
+        private val TAG = Rxjava2Activity::class.java.simpleName
+    }
 }
